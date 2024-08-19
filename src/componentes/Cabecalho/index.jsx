5e87109be9cbd84e2479382af73a354d08980b30
João Victor <joaovictormdasilva676@gmail.com>
2024-08-19 19:00:31 -0300

    Header lg

---
+++ src/componentes/Cabecalho/index.jsx
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+import CampoDeTexto from "../CampoDeTexto";
+
+const HeaderEstilizado = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
+`;
+
+const Cabecalho = () => {
+    return (
+        <HeaderEstilizado>
+            <img src="/imagens/logo.png" alt="Logo" />
+            <CampoDeTexto />
+        </HeaderEstilizado>
+    );
+};
+export default Cabecalho;
